48499c42738b4ef979a877f04d70909127c915bb
dcbastien <ddcbastien@gmail.com>
2021-11-29 14:05:47 -0600

    Added Bonus and Notes from class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import MCUShows from "./mcu-shows/MCUShows";
 
+// BONUS 6a & 8a
+import { useState, useEffect } from 'react';
+
 
 const releaseDates = {
   wandaVision: 'Jan 2021',
@@ -9,14 +12,64 @@ const releaseDates = {
   hawkeye: 'November 2021'
  }
 
+// BONUS 6b
+const mcuCharacters = [
+  'Scarlet Witch',
+  'Vision',
+  'Falcon',
+  'Winter Soldier',
+  'Loki',
+  'The Watcher',
+  'Hawkeye'
+];
 
 function App() {
+
+  // BONUS 6c
+  const [index, setIndex] = useState(0);
+
+  // BONUS 7a
+  const [num, setNum] = useState(0);
+
+  // BONUS 8a
+  // The function (1st argument) passed into the useEffect function/hook will run every time the state value ("num" in this example) that is passed into the array (2nd argument) is updated.
+  // NOTE: If nothing is passed in for a 2nd argument, then the function will run whenever any state values are updated.)
+  useEffect(() => {
+    alert("The Next MCU Character has been displayed");
+  }, [num]);
+
+  // BONUS 6f
+  function newIndex() {
+    const randomIndex = Math.floor(Math.random() * mcuCharacters.length);
+    setIndex(randomIndex);
+  }
+
+  // BONUS 7d
+  function newNum() {
+    if (num < mcuCharacters.length - 1) {
+      setNum(num + 1);
+    } else {
+      setNum(0);
+    }
+  }
+
   return (
     <div>
       <h1>FINAL REACT EXERCISE</h1>
-  <MCUShows dates = {releaseDates} />  
+ {/* 2c & 5b & BONUS 6f & 7d */}
+ <MCUShows
+        dates={releaseDates}
+        indexTwo={newIndex}
+        numTwo={newNum}
+      />
+    {/* BONUS 6d & 6f */}
+    <h1>Random MCU Character: {mcuCharacters[index]}</h1>
+      {/* BONUS 7b & 7d */}
+      <h1>Next MCU Character: {mcuCharacters[num]}</h1>  
   </div>
   );
 }
 
 export default App;
+
+
--- src/mcu-shows/MCUShows.js
+++ src/mcu-shows/MCUShows.js
@@ -2,7 +2,9 @@ import './MCUShows.css';
 
 
 
-function MCUShows ({dates}) {
+
+// 1b & 5c & BONUS 6f & 7d
+function MCUShows({dates, indexTwo, numTwo}){
     return (
        <div className='MCUShows'>
            <h1 style={{fontFamily: `Georgia`, textDecoration: `underline` }}>Disney+</h1>
@@ -12,9 +14,12 @@ function MCUShows ({dates}) {
            <p>Loki: {dates.loki} </p>
            <p>What If...?: {dates.whatIf}</p>
            <p>Hawkeye: {dates.hawkeye} </p>
-       </div> 
-
-    );
+       {/* BONUS 6e & 6f */}
+      <button onClick={indexTwo}>RANDOM MCU CHARACTER</button>
+      {/* BONUS 7c & 7d */}
+      <button onClick={numTwo}>NEXT MCU CHARACTER</button>
+    </div>
+  );
 }
 
 
